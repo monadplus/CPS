09021f2be91ab42e6da4aa6edaa51ffc6368fee6
monadplus <arnauabella@gmail.com>
2020-05-21 11:15:39 +0200

    lp document + tar prepared

--- box_wrapping/cp/shell.nix
+++
@@ -1,6 +0,0 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "myenv";
-  buildInputs = [ zlib gecode ];
-}
---
+++ box_wrapping/lp/src/box.cpp
@@ -0,0 +1,122 @@
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ilcplex/ilocplex.h>
+
+ILOSTLBEGIN
+
+bool cmp(const pair<int,int>& b1, const pair<int,int>& b2) {
+  return (b1.first * b1.second) > (b2.first * b2.second);
+}
+
+int main(int argc, char* argv[]) {
+
+  IloEnv env;
+
+  try {
+    if (argc != 1) return -1;
+    IloInt w, n; // width and total number of boxes
+    IloInt l = 0; // max length
+    cin >> w >> n;
+    cout << w << " " << n << endl;
+    vector<pair<int,int>> boxes(n);
+    int k = 0;
+    while (k < n) {
+      int m, width, height;
+      cin >> m >> width >> height;
+      cout << m << "   " << width << " " << height << endl;
+      for (int i = 0; i < m; ++i) {
+        boxes[k+i] = pair<int,int>(width,height);
+        l += max(width,height);
+      }
+      k += m;
+    }
+
+    // Try placing the biggest boxes first
+    // nb. not sure if this works in CPLEX.
+    sort(boxes.begin(), boxes.end(), cmp);
+
+    IloModel model(env);
+
+    IloNumVarArray x_tl  = IloNumVarArray(env, boxes.size(), 0, w-1);
+    IloNumVarArray y_tl  = IloNumVarArray(env, boxes.size(), 0, l-1);
+    // nb. bottom is no longer required.
+
+    // Symmetry
+    x_tl[0].setBounds(0,0);
+    y_tl[0].setBounds(0,0);
+
+    IloNumVarArray width  = IloNumVarArray(env, boxes.size(), 0, w);
+    IloNumVarArray height = IloNumVarArray(env, boxes.size(), 0, l-1);
+
+    IloNumVar length(env, 0, l-1);
+
+    IloInt i;
+    IloInt j;
+
+    for(i = 0; i < boxes.size(); i++) {
+      IloInt b_width  = boxes[i].first;
+      IloInt b_height = boxes[i].second;
+
+      // length
+      model.add(length >= y_tl[i] + height[i] - 1);
+
+      // x bounds
+      model.add(x_tl[i] <= w - width[i]);
+
+      // width & height
+      if (b_width == b_height) {
+        width[i].setBounds(b_width, b_width);
+        height[i].setBounds(b_height, b_height);
+      }
+      else {
+        model.add(width[i] == b_width || width[i] == b_height);
+        model.add(height[i] == b_width || height[i] == b_height);
+        model.add(width[i] != height[i]);
+      }
+
+      // overlapping
+      for (j = i+1; j < boxes.size(); j++) {
+        model.add(  (x_tl[i]+width[i]  <= x_tl[j])
+                 || (x_tl[i]           >= x_tl[j]+width[j])
+                 || (y_tl[i]+height[i] <= y_tl[j])
+                 || (y_tl[i]           >= y_tl[j]+height[j]));
+      }
+    }
+
+    // Objective Function
+    model.add(IloMinimize(env, length));
+
+    IloCplex cplex(model);
+    cplex.setOut(env.getNullStream());
+
+    if (cplex.solve()) {
+       cout << (cplex.getObjValue() + 1) << endl;
+       for (i = 0; i < boxes.size(); ++i) {
+         IloNum x_i = cplex.getValue(x_tl[i]);
+         IloNum y_i = cplex.getValue(y_tl[i]);
+         IloNum w_i = cplex.getValue(width[i]);
+         IloNum h_i = cplex.getValue(height[i]);
+         cout << IloRound(x_i) << " " << IloRound(y_i)
+              << "   "
+              << IloRound(x_i+w_i-1) << " " << IloRound(y_i+h_i-1)
+              << endl;
+       }
+    }
+    else {
+       cout << " No solution found" << endl;
+    }
+  }
+  catch (IloException& ex) {
+     cerr << "Error: " << ex << endl;
+  }
+  catch (...) {
+     cerr << "Error" << endl;
+  }
+
+  env.end();
+  return 0;
+}
