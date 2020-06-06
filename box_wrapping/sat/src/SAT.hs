a0d2bb36fdcaeac3b924ac0d12e3760475e643b2
monadplus <arnauabella@gmail.com>
2020-06-06 15:23:31 +0200

    overlapping

--- box_wrapping/sat/src/SAT.hs
+++ box_wrapping/sat/src/SAT.hs
@@ -39,6 +39,18 @@ data Box = Box
   deriving stock (Show)
 makeLenses ''Box
 
+instance Eq Box where
+  b1 == b2 =
+    b1^.width == b2^.width
+      && b1^.height == b2^.height
+
+-- By total area
+instance Ord Box where
+  b1 `compare` b2 =
+    let area b = b^.width * b^.height
+     in area b1 `compare` area b2
+
+
 getMaxLength :: [Box] -> Int
 getMaxLength = maximum . fmap getLength
   where
@@ -101,8 +113,16 @@ type SAT = StateS ()
 neg :: Variable -> Variable
 neg = Variable . negate . variable
 
+negMaybe :: Maybe Variable -> Maybe Variable
+negMaybe Nothing = Nothing
+negMaybe (Just variable) = Just (neg variable)
+
 emptyClause :: Clause
 emptyClause = Clause []
+
+maybeClause :: Maybe Variable -> Clause
+maybeClause Nothing         = emptyClause
+maybeClause (Just variable) = Clause [variable]
 
 addClause :: Clause -> SAT
 addClause c = clauses %= (c :)
@@ -198,15 +218,15 @@ insideTheBounds = do
                      var x y = bVar s (x,y) b
                      rot = s ^?! rotations . ix b
 
-                     -- When width = height, there is no need to test rotation.
-                     fixRotation =
-                       if box^.width == box^.height then
-                         \clauses -> (neg rot \/ emptyClause) : clauses
-                       else
-                         id
+                     -- 1) When width = height, there is no need to test rotation.
+                     --    We can set it to rotated and just add the negation of each position (x,y)
+                     fixRotation
+                       | box^.width == box^.height =
+                          \clauses -> (neg rot \/ emptyClause) : clauses
+                       | otherwise = id
 
                      cs = catMaybes [ computeClause (x,y) box _w _maxLength rot (var x y)
-                                    | x <- [0..(_w - 1)]
+                                    | x <- [0..        (_w - 1)]
                                     , y <- [0..(_maxLength - 1)]
                                     ]
 
@@ -214,34 +234,88 @@ insideTheBounds = do
 
                | b <- [0..(length _boxes - 1)]
                ]
+  -- Add all clauses to the CNF.
   addClauses boundsClauses
 
   where
     computeClause :: Coord -> Box -> Int -> Int -> Variable -> Variable -> Maybe Clause
-    computeClause (x,y) box w maxLength rot var =
-       -- No rotation
-       if box^.width == box^.height then
-         if x > w - box^.width || y > maxLength - box^.height
-             then Just $ neg var \/ emptyClause
-         else Nothing
+    computeClause (x,y) box w maxLength rot var
+       -- See 1)
+       | box^.width == box^.height =
+           if x > w - box^.width || y > maxLength - box^.height
+               then Just $ neg var \/ emptyClause
+           else Nothing
 
-       -- May rotate
-       else
-         -- Not rotated
-         if x > w - box^.width || y > maxLength - box^.height
-           then Just $ rot \/ neg var
-         -- Rotated
-         else if x > w - box^.height || y > maxLength - box^.width
-           then Just $ neg rot \/ neg var
-         else Nothing
+       -- May rotate: we need to add clauses for both positions.
+       | otherwise =
+           -- 1.- Not rotated
+           if x > w - box^.width || y > maxLength - box^.height
+             then Just $ rot \/ neg var
+           -- 2.- Rotated
+           else if x > w - box^.height || y > maxLength - box^.width
+             then Just $ neg rot \/ neg var
+           else Nothing
 
+-- | For all boxes b1, b2 (b1 < b2) computes the overlapping region form the following implication:
+--
+--    var(b,(x,y)) ----> neg var(b2,(i,j))
+--
+-- (x,y) is the traversal of the whole roll paper (except for the margins)
+-- (i,j) is the area where the two boxes would collide.
+--
+-- We also take into account all possible rotations of b1 and box2.
+--
+noOverlapping :: SAT
+noOverlapping = do
+  s@S{..} <- get
+  let clauses =
+        concat [ computeClauses s b1 w1 h1 rot1 ++ computeClauses s b1 h1 w1 (negMaybe rot1)
+                  | b1 <- [0..length _boxes - 1]
+                  , let w1 = s ^?! boxes . ix b1 . width
+                  , let h1 = s ^?! boxes . ix b1 . height
+                  , let rot1 | w1 == h1  = Nothing
+                             | otherwise = Just (s ^?! rotations . ix b1)
+               ]
+  addClauses clauses
+
+  where
+
+    computeClauses :: S -> Int -> Int -> Int -> Maybe Variable -> [Clause]
+    computeClauses s@S{..} b1 w1 h1 rot1 =
+      concat $
+        [ computeClauses' (x,y) b2 w2 h2 rot2 ++ computeClauses' (x,y) b2 h2 w2 (negMaybe rot2)
+            | x  <- [0.._w  - w1]
+            , y  <- [0.._maxLength - h1]
+            , b2 <- [b1+1..(length _boxes - 1)]
+            , let w2 = s ^?! boxes . ix b2 . width
+            , let h2 = s ^?! boxes . ix b2 . height
+            , let rot2 | w2 == h2  = Nothing
+                       | otherwise = Just (s ^?! rotations . ix b2)
+        ]
+
+      where
+
+        computeClauses' :: Coord -> Int -> Int -> Int -> Maybe Variable -> [Clause]
+        computeClauses' (x,y) b2 w2 h2 rot2 =
+          [ maybeClause rot1 \/ maybeClause rot2 \/ neg boxVar1 \/ neg boxVar2
+              | i <- [max 0 (x - w2 + 1)  ..  min _w (x + w1 - 1)]
+              , j <- [max 0 (y - h2 + 1)  ..  min _w (y + h1 - 1)]
+              , let boxVar1 = bVar s (x,y) b1
+              , let boxVar2 = bVar s (i,j) b2
+          ]
+
+
+-- TODO
+-- TODO
+-- TODO
+-- TODO
 -- TODO
 buildSAT :: SAT
 buildSAT = do
   -- Symmetry: first box on (0,0) [the biggest one]
   exactlyOne
   insideTheBounds
-  -- Overlapping
+  noOverlapping
 
 ----------------------------------------------
 -- SAT -> BWP
@@ -273,6 +347,15 @@ type RawBoxSol = [Int]   -- ^ Length = w * maxLength
 type RawRotsSol = [Int]  -- ^ Length = #boxes
 type RawRotSol = Int
 
+--     --- TODO
+--
+--     The upper bound on the length of the roll can also be improved. The
+--     one you have now corresponds to placing the boxes one on top of the
+--     other. But you can exploit that there may be many identical boxes. For
+--     example, you can put identical rectangles one *next* to each other
+--     until they fill the whole width, and only then begin a new line. In
+--     this way you will get an upper bound which is always as good as the
+--     one you have now, and usually strictly better.
 getLen :: [BoxSol] -> Int
 getLen =
   (+) 1 . foldr (\BoxSol{..} -> max (snd br)) 0
